ecc18268db31b2826da0fb0ff52b90b034d7bcdb
alberthsantiagoleal-cmyk <alberthsantiagoleal@gmail.com>
2026-02-02 16:53:47 -0500

    euler

---
+++ euler.py
@@ -0,0 +1,15 @@
+# 3. Dado que el valor de ex se puede calcular por aproximación de la siguiente suma:
+# e^x = 1 + x + x^2/2! + x^3/3! + x^4/4! + x^5/5! + ...
+
+# El usuario ingresa x
+x = float(input("Ingrese un número x: "))
+
+suma = 1
+termino = 1
+
+for i in range(1, 10):   # 10 sumas sencillas
+    termino = termino * x / i
+    suma = suma + termino
+
+print("El valor aproximado de e^x es:", suma)
+
